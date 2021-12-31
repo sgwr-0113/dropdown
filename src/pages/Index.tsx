a8798d0025848ef64ee7eb2a9bc71cc9360ebe64
sgwr-0113 <xxepisode.18xx@gmail.com>
2021-12-31 16:35:11 +0900

    Revert "eslint"
    This reverts commit beab200b649a12ceaccdb534396399add3b5157c.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,9 +14,6 @@ export const Index = () => {
     console.log(`コメント${comment}に変わったよ!`);
   }, [comment])
 
-  // useCallbackは値を保持するもの
-  // 第二引数[]には再描画の条件（「これが変更になったら再描画する」というもの）を書く
-  // 主に子コンポーネントにpropsで関数を渡す場合に使う
   const handleSetUserName = useCallback((value: string) => {
     setComment(value)
   }, []);
