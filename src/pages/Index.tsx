830254a53fc86a71c2b9aee115266313b13bfc0b
sgwr-0113 <xxepisode.18xx@gmail.com>
2021-12-31 15:39:12 +0900

    add comment

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,19 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import  { DropdownList } from '../components/DropdownList';
 import { ArtType, UserType } from '../types/user';
 import { Users, Arts } from '../mocks/data';
-
 
 export const Index = () => {
   const users: UserType[] = Users;
   const arts: ArtType[] = Arts;
   const [comment, setComment] = useState<string>("コメント");
   const [price, setPrice] = useState<number>(0);
-  
+
+  // useEffectは第二引数[]内の値が変わるたびに実行される
+  useEffect(()=>{
+    console.log(`コメント${comment}に変わったよ!`);
+  }, [comment])
+
   const handleSetUserName = useCallback((value: string) => {
     setComment(value)
   }, []);
