b7ac20b36f49542954761717d630416a813d467a
sgwr-0113 <xxepisode.18xx@gmail.com>
2022-01-16 00:40:17 +0900

    apply components

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,20 @@ import React, { useState, useCallback, useEffect } from 'react';
 import  { DropdownList } from 'components/DropdownList';
 import { ListItemType } from 'types/types';
 import { Users, Arts } from 'mocks/data';
-import style from 'stylesheets/Index.module.scss';
+// import style from 'stylesheets/Index.module.scss';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+// styled方式で既存のコンポーネントをカスタマイズできる
+const MyPaper = styled(Paper)({
+  width: 360,
+  height: 360,
+  margin: 16,
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+})
 
 export const Index = () => {
   const users: ListItemType<string>[] = Users;
@@ -23,17 +36,28 @@ export const Index = () => {
   }, []);
 
   return (
-    <div className={style["wrapper"]}>
-      <div className={style["card"]}>
+    <Box
+    sx={{
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& h1': {
+        color: 'primary.main',
+      },
+      '& h2': {
+        color: 'secondary.main',
+      }
+    }}
+  >
+      <MyPaper elevation={3}>
         <h1>コメント「{comment}」</h1>
         <h2>ユーザー一覧</h2>
         <DropdownList<string> listItems={users} setListItem={handleSetUserComment} />
-      </div>
-      <div className={style["card"]}>
+      </MyPaper>
+      <MyPaper elevation={3}>
         <h1>価格 {price}円</h1>
         <h2>作品一覧</h2>
         <DropdownList<number> listItems={arts} setListItem={handleSetArtPrice} />
-      </div>
-    </div>
+      </MyPaper>
+    </Box>
   );
 };
