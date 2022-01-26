b436653aa177a7d22a9ce1716d8c88825d9d0f01
sgwr-0113 <xxepisode.18xx@gmail.com>
2022-01-26 23:09:15 +0900

    prettier

--- src/components/DropdownList.tsx
+++ src/components/DropdownList.tsx
@@ -1,29 +1,22 @@
-import React from "react";
-import { DropdownItem } from "components/DropdownItem";
-import { ListItemType } from "types/types";
-import Stack from "@mui/material/Stack";
+import React from 'react'
+import { DropdownItem } from 'components/DropdownItem'
+import { ListItemType } from 'types/types'
+import Stack from '@mui/material/Stack'
 
 interface Props<T> {
-  listItems: ListItemType<T>[];
-  setListItem: (value: T) => void;
+  listItems: ListItemType<T>[]
+  setListItem: (value: T) => void
 }
 
 export const DropdownList = <T,>(props: Props<T>) => {
-  const { listItems, setListItem } = props;
+  const { listItems, setListItem } = props
 
   return (
     // sxを用いると簡易表記が可能になる pはpadding
-    <Stack spacing={2} sx={{ p: "20px 0" }}>
+    <Stack spacing={2} sx={{ p: '20px 0' }}>
       {listItems.map((listItem, i) => {
-        return (
-          <DropdownItem<T>
-            key={i}
-            name={listItem.name}
-            value={listItem.value}
-            setValue={setListItem}
-          />
-        );
+        return <DropdownItem<T> key={i} name={listItem.name} value={listItem.value} setValue={setListItem} />
       })}
     </Stack>
-  );
-};
+  )
+}
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useCallback, useEffect } from 'react';
-import  { DropdownList } from 'components/DropdownList';
-import { ListItemType } from 'types/types';
-import { Users, Arts } from 'mocks/data';
-import style from 'stylesheets/Index.module.scss';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import { Typography } from '@mui/material';
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import React, { useState, useCallback, useEffect } from 'react'
+import { DropdownList } from 'components/DropdownList'
+import { ListItemType } from 'types/types'
+import { Users, Arts } from 'mocks/data'
+import style from 'stylesheets/Index.module.scss'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import { styled } from '@mui/material/styles'
+import { Typography } from '@mui/material'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline'
+import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined'
 
 // styled方式で既存のコンポーネントをカスタマイズできる
 const MyPaper = styled(Paper)({
@@ -22,55 +22,55 @@ const MyPaper = styled(Paper)({
 })
 
 export const Index = () => {
-  const users: ListItemType<string>[] = Users;
-  const arts: ListItemType<number>[] = Arts;
-  const [comment, setComment] = useState<string>("");
-  const [price, setPrice] = useState<number>(0);
+  const users: ListItemType<string>[] = Users
+  const arts: ListItemType<number>[] = Arts
+  const [comment, setComment] = useState<string>('')
+  const [price, setPrice] = useState<number>(0)
 
   // useEffectは第二引数[]内の値が変わるたびに実行される
-  useEffect(()=>{
-    console.log(`コメント${comment}に変わったよ!`);
+  useEffect(() => {
+    console.log(`コメント${comment}に変わったよ!`)
   }, [comment])
 
   const handleSetUserComment = useCallback((value: string) => {
     setComment(value)
-  }, []);
+  }, [])
   const handleSetArtPrice = useCallback((value: number) => {
     setPrice(value)
-  }, []);
+  }, [])
 
   return (
     <Box
-    sx={{
-      height: '100vh',
-      width: '100vw',
-      display: 'flex',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      flexWrap: 'wrap',
-      '& h5': {
-        color: 'primary.main',
-        lineHeight: 3,
-      },
-      '& h6': {
-        color: 'secondary.light',
-        lineHeight: 2,
-        fontWeight: 700,
-      },
-    }}
-  >
-      <MyPaper elevation={3} className={style["paper"]}>
-        <ChatBubbleOutlineIcon className={style["icon"]} color="primary" fontSize="large" />
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        '& h5': {
+          color: 'primary.main',
+          lineHeight: 3,
+        },
+        '& h6': {
+          color: 'secondary.light',
+          lineHeight: 2,
+          fontWeight: 700,
+        },
+      }}
+    >
+      <MyPaper elevation={3} className={style['paper']}>
+        <ChatBubbleOutlineIcon className={style['icon']} color="primary" fontSize="large" />
         <Typography variant="h5">「{comment}」</Typography>
         <Typography variant="h6">ユーザー一覧</Typography>
         <DropdownList<string> listItems={users} setListItem={handleSetUserComment} />
       </MyPaper>
-      <MyPaper elevation={3} className={style["paper"]}>
-        <PaidOutlinedIcon className={style["icon"]} color="primary" fontSize='large' />
+      <MyPaper elevation={3} className={style['paper']}>
+        <PaidOutlinedIcon className={style['icon']} color="primary" fontSize="large" />
         <Typography variant="h5">{price}円</Typography>
         <Typography variant="h6">作品一覧</Typography>
         <DropdownList<number> listItems={arts} setListItem={handleSetArtPrice} />
       </MyPaper>
     </Box>
-  );
-};
+  )
+}
